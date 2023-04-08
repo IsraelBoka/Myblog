14a9503df69c33698b97207671d1036d521a0a18
IsraelBoka <108950893+IsraelBoka@users.noreply.github.com>
2023-04-08 16:53:20 +0200

    form submit button

--- resources/views/form1.blade.php
+++ resources/views/form1.blade.php
@@ -9,7 +9,7 @@
             
         <div class="">
             <form class="">
-              <div class="space-y-12">
+              <div class="space-y-12 pb-8">
                 <div class="border-b border-gray-900/10 pb-12">
                   <h2 class="text-base font-semibold leading-7 text-gray-900">Article</h2>
                   <p class="mt-1 text-sm leading-6 text-gray-600">
@@ -62,6 +62,12 @@
                     <h2 class="block text-sm font-medium leading-6 text-gray-900">Contenu</h2>
                     <textarea id="description" name="description" class="block lg:w-96 rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"></textarea>
 
+                </div>
+
+                <div>
+                    <x-button>
+                        {{ __('Enregistrer') }}
+                    </x-button>
                 </div>
             
               </div>
